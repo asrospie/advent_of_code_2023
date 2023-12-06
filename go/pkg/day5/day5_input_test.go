466e2dce651ff51c65709231e79cc82bc68349b8
asrospie <asrospierski@gmail.com>
2023-12-06 09:23:39 -0500

    update test case

--- go/pkg/day5/day5_input_test.go
+++ go/pkg/day5/day5_input_test.go
@@ -19,6 +19,18 @@ func TestDay5Part1Input(t *testing.T) {
 }
 
 func TestDay5Part2Input(t *testing.T) {
+    t.Skip("Solution takes too long to run. See next test for faster solution.")
+    num, err := Day5Part2("../../inputs/day_5_input.txt")
+    if err != nil {
+        t.Error(err)
+    }
+    expected := 34039469
+    if num != expected {
+        t.Errorf("Expected %d, got %d", expected, num)
+    }
+}
+
+func TestDay5Part2InputReverse(t *testing.T) {
     if testing.Short() {
         t.Skip("Skipping test in short mode.")
     }
