6e6de42f3fad26e9691f6c65df7627c96aa9c4ab
asrospie <asrospierski@gmail.com>
2023-12-07 12:54:17 -0500

    part 2 attempt fail

--- go/pkg/day7/day7.go
+++ go/pkg/day7/day7.go
@@ -1,11 +1,12 @@
 package day7
 
 import (
+	"errors"
 	"fmt"
 	utils "rospierski/aocgo/pkg/aocutils"
-    "strings"
-    "strconv"
-    "sort"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 const (
@@ -29,27 +30,15 @@ func (h *Hand) String() string {
     return fmt.Sprintf("%d %d %d", h.cards, h.kind, h.bid)
 }
 
-func newHand(line string) (Hand, error) {
-    str_split := strings.Split(line, " ")
-    cards_str, bid_str := str_split[0], str_split[1]
-    card_bin := make(map[rune]int)
-    for _, c := range cards_str {
-        card_bin[c]++
-    }
-
-    og_cards_str := cards_str
-    cards_str = strings.ReplaceAll(cards_str, "A", "E")
-    cards_str = strings.ReplaceAll(cards_str, "K", "D")
-    cards_str = strings.ReplaceAll(cards_str, "Q", "C")
-    cards_str = strings.ReplaceAll(cards_str, "J", "B")
-    cards_str = strings.ReplaceAll(cards_str, "T", "A")
-
-    decimal_from_hex, err := strconv.ParseInt(cards_str, 16, 64)
+func hexHandToInt(hand string) (int, error) {
+    decimal_from_hex, err := strconv.ParseInt(hand, 16, 64)
     if err != nil {
-        return Hand{}, err
+        return -1, err
     }
-    cards := int(decimal_from_hex)
-    bid_num, err := strconv.Atoi(bid_str)
+    return int(decimal_from_hex), nil
+}
+
+func getHandKind(card_bin map[rune]int) int {
     kind := -1
     switch len(card_bin) {
     case 5:
@@ -81,6 +70,34 @@ func newHand(line string) (Hand, error) {
     case 1:
         kind = FIVE_KIND
     }
+    return kind
+}
+
+func newHand(line string, face_card_map map[string]string) (Hand, error) {
+    str_split := strings.Split(line, " ")
+    cards_str, bid_str := str_split[0], str_split[1]
+    card_bin := make(map[rune]int)
+    for _, c := range cards_str {
+        card_bin[c]++
+    }
+
+    og_cards_str := cards_str
+    cards_str = strings.ReplaceAll(cards_str, "A", face_card_map["A"])
+    cards_str = strings.ReplaceAll(cards_str, "K", face_card_map["K"])
+    cards_str = strings.ReplaceAll(cards_str, "Q", face_card_map["Q"])
+    cards_str = strings.ReplaceAll(cards_str, "J", face_card_map["J"])
+    cards_str = strings.ReplaceAll(cards_str, "T", face_card_map["T"])
+
+    decimal_from_hex, err := hexHandToInt(cards_str)
+    if err != nil {
+        return Hand{}, err
+    }
+    cards := int(decimal_from_hex)
+    bid_num, err := strconv.Atoi(bid_str)
+    kind := getHandKind(card_bin)
+    if kind == -1 {
+        return Hand{}, errors.New("invalid hand")
+    }
     return Hand{
         cards,
         og_cards_str,
@@ -90,9 +107,16 @@ func newHand(line string) (Hand, error) {
 }
 
 func getHands(lines []string) ([]Hand, error) {
+    face_card_map := map[string]string{
+        "A": "E",
+        "K": "D",
+        "Q": "C",
+        "J": "B",
+        "T": "A",
+    }
     hands := make([]Hand, len(lines))
     for i, line := range lines {
-        hand, err := newHand(line)
+        hand, err := newHand(line, face_card_map)
         if err != nil {
             return nil, err
         }
@@ -123,6 +147,115 @@ func Day7Part1(filename string) (int, error) {
     sum := 0
     for i, hand := range hands {
         sum += hand.bid * (i + 1)
+    }
+
+	return sum, nil
+}
+
+func getHandsJoker(lines []string) ([]Hand, error) {
+    hands := make([]Hand, len(lines))
+    for i, line := range lines {
+        hand, err := newHandJoker(line)
+        if err != nil {
+            return nil, err
+        }
+        hands[i] = hand
+    }
+    return hands, nil
+}
+
+func newHandJoker(line string) (Hand, error) {
+    face_card_map := map[string]string{
+        "A": "E",
+        "K": "D",
+        "Q": "C",
+        "J": "1",
+        "T": "A",
+    }
+    hand, err := newHand(line, face_card_map) 
+    if err != nil {
+        return Hand{}, err
+    }
+
+    joker_count := strings.Count(hand.cards_str, "J")
+    new_kind := hand.kind
+    if joker_count > 0 {
+        switch hand.kind {
+        case HIGH_CARD:
+            new_kind = ONE_PAIR
+        case ONE_PAIR:
+            new_kind = THREE_KIND
+        case TWO_PAIR:
+            if joker_count == 1 {
+                new_kind = FULL_HOUSE
+            } else {
+                new_kind = FOUR_KIND
+            }
+        case THREE_KIND:
+            new_kind = FOUR_KIND
+        case FULL_HOUSE:
+            new_kind = FIVE_KIND
+        case FOUR_KIND:
+            if joker_count == 1 {
+                new_kind = FIVE_KIND
+            } 
+        default:
+            new_kind = hand.kind
+        }
+    }
+    if new_kind != hand.kind {
+        fmt.Printf("%s %s %s\n", hand.cards_str, kindToString(hand.kind), kindToString(new_kind))
+    }
+    hand.kind = new_kind
+    return hand, nil
+}
+
+func kindToString(kind int) string {
+    switch kind {
+    case HIGH_CARD:
+        return "HIGH_CARD"
+    case ONE_PAIR:
+        return "ONE_PAIR"
+    case TWO_PAIR:
+        return "TWO_PAIR"
+    case THREE_KIND:
+        return "THREE_KIND"
+    case FULL_HOUSE:
+        return "FULL_HOUSE"
+    case FOUR_KIND:
+        return "FOUR_KIND"
+    case FIVE_KIND:
+        return "FIVE_KIND"
+    }
+    return "INVALID"
+}
+
+func Day7Part2(filename string) (int, error) {
+	lines, err := utils.ReadFileLines(filename)
+	if err != nil {
+		return -1, err
+	}
+
+    hands, err := getHandsJoker(lines)
+    if err != nil {
+        return -1, err
+    }
+
+    // sort hands
+    sort.Slice(hands, func(i, j int) bool {
+        if hands[i].kind == hands[j].kind {
+            return hands[i].cards < hands[j].cards
+        }
+        return hands[i].kind < hands[j].kind
+    })
+
+    sum := 0
+    for i, hand := range hands {
+        sum += hand.bid * (i + 1)
+    }
+
+    for _, hand := range hands {
+        fmt.Println(hand)
     }
 
 	return sum, nil
--- go/pkg/day7/day7_test.go
+++ go/pkg/day7/day7_test.go
@@ -25,3 +25,26 @@ func TestDay7Part1Input(t *testing.T) {
 		t.Errorf("Expected %d, got %d", expected, num)
 	}
 }
+
+func TestDay7Part2Example(t *testing.T) {
+	num, err := Day7Part2("../../inputs/day_7_example.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	expected := 5905
+	if num != expected {
+		t.Errorf("Expected %d, got %d", expected, num)
+	}
+}
+
+
+func TestDay7Part2Input(t *testing.T) {
+	num, err := Day7Part2("../../inputs/day_7_input.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	expected := 251058093
+	if num != expected {
+		t.Errorf("Expected %d, got %d", expected, num)
+	}
+}
