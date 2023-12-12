0e23179631d35206d53e9111bc8e57938ba94e3a
asrospie <asrospierski@gmail.com>
2023-12-12 13:48:47 -0500

    Finish faster version for part 2 -- thanks to HyperNeutrino on YouTube for explaining how to think of the problem

--- go/pkg/day12/day12.go
+++ go/pkg/day12/day12.go
@@ -1,11 +1,12 @@
 package day12
 
 import (
-    "fmt"
-    utils "rospierski/aocgo/pkg/aocutils"
-    "strings"
-    "strconv"
-    "regexp"
+	"fmt"
+	"regexp"
+	utils "rospierski/aocgo/pkg/aocutils"
+	"strconv"
+	"strings"
+	"sync"
 )
 
 func parseInput(lines []string) ([]string, [][]int) {
@@ -48,22 +49,31 @@ func findPermutations(line string, record []int) int {
     re := regexp.MustCompile(re_str)
 
     sum := 0
-    for i := 0; i <= max_bin_num; i++ {
-        temp := binary_str
-        test := fmt.Sprintf("%b", i)
-        for j := len(test); j < unknown_len; j++ {
-            test = "0" + test
-        }
-        for _, c := range strings.Split(test, "") {
-            temp = strings.Replace(temp, "?", c, 1) 
-        }
-        // check match
-        match := re.FindString(temp)
-        if match != "" && len(match) == len(temp) {
-            sum++
-        }
-    }
 
+    sums := make([]int, max_bin_num + 1)
+    wg := sync.WaitGroup{}
+    for i := 0; i <= max_bin_num; i++ {
+        wg.Add(1)
+        go func(i int, max_bin_num int, unknown_len int, binary_str string, re *regexp.Regexp) {
+            defer wg.Done()
+            temp := binary_str
+            test := fmt.Sprintf("%b", i)
+            for j := len(test); j < unknown_len; j++ {
+                test = "0" + test
+            }
+            for _, c := range strings.Split(test, "") {
+                temp = strings.Replace(temp, "?", c, 1) 
+            }
+            // check match
+            match := re.FindString(temp)
+            if match != "" && len(match) == len(temp) {
+                sums[i]++
+            }
+        }(i, max_bin_num, unknown_len, binary_str, re)
+    }
+    wg.Wait()
+    sum = utils.SumSlice(sums)
+    fmt.Println(sums)
     return sum
 }
 
@@ -82,6 +92,59 @@ func Day12Part1(filename string) (int, error) {
     return sum, nil
 }
 
+func findPermutationsRec(line string, record []int, cache *map[string]int) int {
+    if len(line) == 0 {
+        if len(record) == 0 {
+            return 1
+        }
+        return 0
+    }
+
+    if len(record) == 0 {
+        if strings.Contains(line, "#") {
+            return 0
+        }
+        return 1
+    }
+
+    key := fmt.Sprintf("%s:%v", line, record)
+    if v, ok := (*cache)[key]; ok {
+        return v
+    }
+
+    total := 0
+
+    cs := []rune(line)
+    c0 := cs[0]
+    if c0 == '.' || c0 == '?' {
+        total += findPermutationsRec(string(cs[1:]), record, cache)
+    }
+
+    if c0 == '#' || c0 == '?' {
+        block_less_than_len := record[0] <= len(cs)
+        block_has_no_dot := !strings.Contains(string(cs[:record[0]]), ".")
+        var tester bool
+        if record[0] >= len(cs) {
+            tester = false
+        } else {
+            tester = cs[record[0]] != '#'
+        }
+        block_has_no_hash := len(cs) == record[0] || tester 
+        if block_less_than_len && block_has_no_dot && block_has_no_hash {
+            var next_cfg string
+            if record[0] >= len(cs) {
+                next_cfg = ""
+            } else {
+                next_cfg = string(cs[(record[0] + 1):])
+            }
+            total += findPermutationsRec(next_cfg, record[1:], cache)
+        }
+    }
+
+    (*cache)[key] = total
+    return total
+}
+
 func Day12Part2(filename string) (int, error) {
     lines, err := utils.ReadFileLines(filename)
     if err != nil {
@@ -89,7 +152,22 @@ func Day12Part2(filename string) (int, error) {
     }
 
     rows, records := parseInput(lines)
+    for i := 0; i < len(rows); i++ {
+        temp_row := rows[i]
+        temp_record := records[i]
+        for j := 0; j < 4; j++ {
+            rows[i] += "?" + temp_row
+            records[i] = append(records[i], temp_record...)
+        }
+    } 
+    
+    cache := make(map[string]int)
 
+    sum := 0
+    for i, row := range rows {
+        sum += findPermutationsRec(row, records[i], &cache)
+    }
+    fmt.Println(sum)
 
-    return -1, nil
+    return sum, nil
 }
--- go/pkg/day12/day12_test.go
+++ go/pkg/day12/day12_test.go
@@ -25,3 +25,25 @@ func TestDay12Part1Input(t *testing.T) {
         t.Errorf("Expected %d, got %d", expected, num)
     }
 }
+
+func TestDay12Part2Example(t *testing.T) {
+    num, err := Day12Part2("../../inputs/day_12_example.txt")
+    if err != nil {
+        t.Error(err)
+    }
+    expected := 525152
+    if num != expected {
+        t.Errorf("Expected %d, got %d", expected, num)
+    }
+}
+
+func TestDay12Part2Input(t *testing.T) {
+    num, err := Day12Part2("../../inputs/day_12_input.txt")
+    if err != nil {
+        t.Error(err)
+    }
+    expected := 18093821750095
+    if num != expected {
+        t.Errorf("Expected %d, got %d", expected, num)
+    }
+}
