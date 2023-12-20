48ba9edb32ebe85500af02b31c92e8bf3cbcd3d4
asrospie <asrospierski@gmail.com>
2023-12-20 01:14:11 -0500

    add file for day 20

---
+++ python/day20.py
@@ -0,0 +1,26 @@
+import re
+
+def part_one(filename):
+    return 0
+
+
+def part_two(filename):
+    return 0
+
+
+def main():
+    example_one = './inputs/day_19_example_1.txt'
+    example_two = './inputs/day_19_example_2.txt'
+    test = './inputs/day_19_input.txt'
+
+    print(f'Part One Example 1: {part_one(example_one)}')
+    print(f'Part One Example 2: {part_one(example_two)}')
+    print(f'Part One Input: {part_one(test)}')
+
+    print(f'Part Two Example 1: {part_two(example_one)}')
+    print(f'Part Two Example 2: {part_two(example_two)}')
+    print(f'Part Two Input: {part_two(test)}')
+
+
+if __name__ == '__main__':
+    main()
